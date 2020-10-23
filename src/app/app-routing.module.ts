405c092f637e415ed2d809a895c7223f6fdf7477
Jade <jademmb@gmail.com>
2020-10-23 23:39:16 +1300

    Added edit icon button and created edit component. -> Hooked up edit icon button to link to the edit page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full'},
   { path: 'products', component: ProductsComponent},
-  { path: 'products/:id', component: ProductDetailComponent}
+  { path: 'products/:id', component: ProductDetailComponent},
+  { path: 'product/edit/:id', component: ProductEditComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import { RouterModule } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 
 @NgModule({
@@ -24,13 +27,16 @@ import { RouterModule } from '@angular/router';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatSelectModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [
     AppComponent,
     ProductsComponent,
     ProductDetailComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductEditComponent
   ],
   bootstrap: [AppComponent],
   providers: []
